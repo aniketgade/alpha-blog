317993d1cd0f3acc3f9aebd2837f1f2e7d7de193
Aniket <agade@osisoft.com>
2021-07-07 13:45:07 -0700

    Adding Localization

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module AlphaBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :ru]  
   end
 end
