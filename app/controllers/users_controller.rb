8be54eb21e8a6c302f611a227d01341c1fade14e
Aniket <agade@osisoft.com>
2021-07-15 16:26:23 -0700

    localize flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to articles_path, notice: t('.notice', user: @user.username)
     else
       render 'new'
     end
