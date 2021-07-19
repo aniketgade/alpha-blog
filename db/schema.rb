ff8b52bf95c0efa12cfe93fc0bcb372dd6d81fc9
Hannah Masila <hannahmasila@gmail.com>
2021-07-18 20:37:51 -0500

    add locale to users for persistent locale setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_173215) do
+ActiveRecord::Schema.define(version: 2021_07_19_001744) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_173215) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "locale", default: "en", null: false
   end
 
 end
