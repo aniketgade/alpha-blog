acfe81349c86658a9cdfb081c01d2ec51abd19c3
Hannah Masila <hannahmasila@gmail.com>
2021-07-18 20:40:37 -0500

    add code for locale setting and switching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  include LocaleHelper
+  before_action :update_user_locale
   before_action :set_locale
+  before_action :redirect_no_locale
 
   private
 
-  def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-        locale :
-        I18n.default_locale
+  def redirect_no_locale
+    return if params[:locale].present? || Rails.env.test?
+
+    redirect_to url_for(params.to_unsafe_h.merge(locale: I18n.locale, only_path: true))
   end
 
   def default_url_options
     { locale: I18n.locale }
   end
-
 end
